32912983e93cda88298121feb2108f71900fc5b5
ckwong <ckwong@esrisingapore.com.sg>
2022-09-20 16:37:20 +0800

    Added missing comma

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -2,22 +2,19 @@ const { loadConfigFromFile, mergeConfig } = require("vite");
 const path = require("path");
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
   },
-  "features": {
-    "storyStoreV7": true
-  }
+  features: {
+    storyStoreV7: true,
+  },
   viteFinal: async (config) => {
     const { config: userConfig } = await loadConfigFromFile(
       path.resolve(__dirname, "../vite.config.ts")
@@ -28,4 +25,4 @@ module.exports = {
       plugins: [],
     });
   },
-}
+};
